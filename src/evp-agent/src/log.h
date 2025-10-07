9a9f9ab42f985a3646e9afb9e663f647e9634e63
JSeimizu <124137997+JSeimizu@users.noreply.github.com>
2025-10-07 13:39:26 +0900

    Update to EDC v0.2.2 (#12)
    * Add EVP agent platform callbacks for logging and memory error handling (#333)
    
    * Add EVP agent platform callbacks for logging and memory error handling
    * fix comment
    * fix include
    * Delete fprint
    * fix log level
    * fix dlog_handler
    * fix log.h
    * fix define name
    * Delete #if !defined(EVP_FILDelete
    ---------
    
    Co-authored-by: JSeimizu <124137997+JSeimizu@users.noreply.github.com>
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    
    * apply clang-format (#338)
    
    * apply clang-format
    * move clang-format file
    ---------
    
    Co-authored-by: Kanji-Uetsugi <uetsugi.kanji@miraxia.com>
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    
    * Update workflow to use senscord v0.1.14.
    
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    
    * Fix EVP Proxy Error Handling (#345)
    
    * Fix snprintf return value handling in proxy cache initialization
    * Fix return value in evp_agent_esf_init_proxy_cache for parameter loading failure
    * Improve error handling in evp_agent_esf_init_proxy_cache and enhance logging functionality
    * Refactor logging macros to use EVP_FILE_NAME for file name retrieval
    * Exec clang-format
    * Fix error code
    
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    
    * Fix sdkenc dependency issue in compliation.
    
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    
    * [ESF] release esf v1.13.8 to update spec document
    
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    
    * Updated edc-sensor to v0.1.14.
    
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    
    * bump version to 0.2.2
    
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    
    ---------
    
    Signed-off-by: Seimizu Joukan <Qingshui.Shangguan@sony.com>
    Co-authored-by: Kanji-Uetsugi <86935733+Kanji-Uetsugi@users.noreply.github.com>
    Co-authored-by: ryotabando <85334396+ryotabando@users.noreply.github.com>
    Co-authored-by: Kanji-Uetsugi <uetsugi.kanji@miraxia.com>
    Co-authored-by: Masashi Koshino <115207604+Koshino-Masashi@users.noreply.github.com>
    Co-authored-by: Ryo Oka <95205614+oka-r@users.noreply.github.com>

--- src/evp-agent/src/log.h
+++ src/evp-agent/src/log.h
@@ -8,12 +8,39 @@
 #define _SSF_ELOG_H__
 
 #include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
 #include <syslog.h>
+
+#include "utility_log.h"
+#include "utility_log_module_id.h"
 
 #define MAX_LOG_STR_LEN 4096
 #define MIN_LOG_STR_LEN 128
 
 #define ELOG_ERR 0xFF
+
+#define EVP_FILE_NAME(file) (strrchr(file, '/') ? strrchr(file, '/') + 1 : file)
+
+#define EVP_AGENT_CRIT(fmt, ...)                                                                \
+    WRITE_DLOG_CRITICAL(MODULE_ID_SYSTEM, "%s %d [CRT]" fmt, EVP_FILE_NAME(__FILE__), __LINE__, \
+                        ##__VA_ARGS__)
+#define EVP_AGENT_ERR(fmt, ...)                                                               \
+    WRITE_DLOG_ERROR(MODULE_ID_SYSTEM, "%s %d [ERR] " fmt, EVP_FILE_NAME(__FILE__), __LINE__, \
+                     ##__VA_ARGS__)
+#define EVP_AGENT_WARN(fmt, ...)                                                             \
+    WRITE_DLOG_WARN(MODULE_ID_SYSTEM, "%s %d [WAR] " fmt, EVP_FILE_NAME(__FILE__), __LINE__, \
+                    ##__VA_ARGS__)
+#define EVP_AGENT_INFO(fmt, ...)                                                             \
+    WRITE_DLOG_INFO(MODULE_ID_SYSTEM, "%s %d [INF] " fmt, EVP_FILE_NAME(__FILE__), __LINE__, \
+                    ##__VA_ARGS__)
+#define EVP_AGENT_DBG(fmt, ...)                                                               \
+    WRITE_DLOG_DEBUG(MODULE_ID_SYSTEM, "%s %d [DBG] " fmt, EVP_FILE_NAME(__FILE__), __LINE__, \
+                     ##__VA_ARGS__)
+#define EVP_AGENT_TRC(fmt, ...)                                                               \
+    WRITE_DLOG_TRACE(MODULE_ID_SYSTEM, "%s %d [TRC] " fmt, EVP_FILE_NAME(__FILE__), __LINE__, \
+                     ##__VA_ARGS__)
+
 /*
  * Priority defined in syslog.h
  *
@@ -27,8 +54,10 @@
  *LOG_DEBUG     :  Debug-level message
  */
 
-void SystemDlog(int priority, const char *tag, const char *file, int line,
-		const char *fmt, ...);
+void SystemDlog(int priority, const char *tag, const char *file, int line, const char *fmt, ...);
+
+void evp_agent_dlog_handler(int lvl, const char *file, int line, const char *fmt, va_list ap,
+                            void *user);
 
 int SystemRegElog(uint8_t component, uint8_t init_value, const char *msg);
 int SystemSetELog(uint8_t component, uint8_t code);
